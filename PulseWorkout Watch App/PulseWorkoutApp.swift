38935631383d50147dec6f1f79c73621c322c739
Robin <robinmurray0307@gmail.com>
2022-12-21 17:40:52 +0000

    Initial Commit

---
+++ PulseWorkout Watch App/PulseWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseWorkoutApp.swift
+//  PulseWorkout Watch App
+//
+//  Created by Robin Murray on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct PulseWorkout_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
